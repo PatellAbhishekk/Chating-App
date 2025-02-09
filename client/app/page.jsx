e1280a7def52b4255007c1e2cb4d961a6b5aaec8
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2025-02-09 11:47:18 +0000

    Add inputs

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,31 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 import { HeroUIProvider } from "@heroui/react";
 import { Messages, Inputs, SignUp } from "@/components";
+import { io } from "socket.io-client";
 
-const socket = io(
-  "https://congenial-zebra-69r46pjwvr6vf6pw-8000.app.github.dev/"
-);
-
-console.log(socket);
+const socket = io("https://madeapp-q3r5v.kinsta.app/");
 
 export default function Home() {
   const [user, setUser] = useState(null);
-  const [imgSrc, setImgSrc] = useState("https://placehold.co/400");
-
-  useEffect(() => {
-    const session = sessionStorage.getItem("user");
-    if (session) {
-      setUser(session);
-    }
-  }, []);
-
-  useEffect(() => {
-    socket.on("image", (data) => {
-      setImgSrc(data);
-    });
-  }, [imgSrc, setImgSrc]);
 
   return (
     <HeroUIProvider>
@@ -35,8 +17,7 @@ export default function Home() {
         ) : (
           <div className="relative min-h-screen max-h-screen">
             <Messages />
-            <img src={imgSrc} />
-            <Inputs socket={socket} />
+            <Inputs socket={socket} name={user} />
           </div>
         )}
       </div>

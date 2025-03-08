2e851b3370a7d54b76854669dc1b98a181995f7b
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2025-03-08 15:48:16 +0000

    Add Some update in typing

--- client/components/Messages/Typing.jsx
+++ client/components/Messages/Typing.jsx
@@ -1,3 +1,14 @@
+"use client";
+import { motion } from "framer-motion";
+
 export default function Typing() {
-  return <div>Typing</div>;
+  return (
+    <motion.div
+      className="text-2xl font-mono font-semibold text-gray-800 dark:text-white"
+      animate={{ opacity: [0, 1, 0] }}
+      transition={{ repeat: Infinity, duration: 1.5 }}
+    >
+      Typing...
+    </motion.div>
+  );
 }
